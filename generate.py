39289e3b067da616eef3de9d8363e2644b21e686
ariG23498 <aritra.born2fly@gmail.com>
2025-05-13 07:03:30 +0200

    resolve merge conflicts

--- generate.py
+++ generate.py
@@ -1,4 +1,4 @@
-import torch
+import torch; torch.manual_seed(0);
 from PIL import Image
 
 from models.vision_language_model import VisionLanguageModel
@@ -12,30 +12,33 @@ elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
 else:
     device = "cpu"
+
 print(f"Using device: {device}")
 
-# Load model from the Hugging Face Hub or a local directory
-model = VisionLanguageModel.from_pretrained("lusxvr/nanoVLM-222M").to(device)
+def generate_tokens(tokens, image):
+    gen = model.generate(tokens, image, max_new_tokens=100)
+    return gen
 
-model.eval()
+if __name__ == "__main__":
+    model = VisionLanguageModel.from_pretrained("lusxvr/nanoVLM-222M").to(device)
+    model.eval()
+    
+    tokenizer = get_tokenizer(model.cfg.lm_tokenizer)
+    image_processor = get_image_processor(model.cfg.vit_img_size)
 
-tokenizer = get_tokenizer(model.cfg.lm_tokenizer)
-image_processor = get_image_processor(model.cfg.vit_img_size)
+    text = "What is this?"
+    template = f"Question: {text} Answer:"
+    encoded_batch = tokenizer.batch_encode_plus([template], return_tensors="pt")
+    tokens = encoded_batch['input_ids'].to(device)
 
-text = "What is this?"
-template = f"Question: {text} Answer:"
-encoded_batch = tokenizer.batch_encode_plus([template], return_tensors="pt")
-tokens = encoded_batch['input_ids'].to(device)
+    image_path = 'assets/image.png'
+    image = Image.open(image_path)
+    image = image_processor(image)
+    image = image.unsqueeze(0).to(device)
 
-image_path = 'assets/image.png'
-image = Image.open(image_path)
-image = image_processor(image)
-image = image.unsqueeze(0).to(device)
-
-print("Input: ")
-print(f'{text}')
-print("Output:")
-num_generations = 5
-for i in range(num_generations):
-    gen = model.generate(tokens, image, max_new_tokens=20)
-    print(f"Generation {i+1}: {tokenizer.batch_decode(gen, skip_special_tokens=True)[0]}")
+    print("Input: ")
+    print(f'{text}')
+    print("Output:")
+    for i in range(5):
+        gen = generate_tokens(tokens, image)
+        print(f"Generation {i+1}: {tokenizer.batch_decode(gen, skip_special_tokens=True)[0]}")

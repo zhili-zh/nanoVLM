26d4790db0a01fbdcafb09675a0906e103d0dcac
lusxvr <luis.wiedmann@gmail.com>
2025-05-16 14:56:35 +0000

    fixed compile

--- models/config.py
+++ models/config.py
@@ -45,12 +45,12 @@ class TrainConfig:
     data_cutoff_idx: int = None
     val_ratio: float = 0.025
     batch_size: int = 256
-    gradient_accumulation_steps: int = 8
+    gradient_accumulation_steps: int = 1
     mmstar_batch_size: int = 32
     eval_in_epochs: bool = True
     eval_interval: int = 250
     epochs: int = 5
-    compile: bool = True
+    compile: bool = False
     resume_from_vlm_checkpoint: bool = False # Indicate if the training should be resumed from a checkpoint of the whole VLM or you want to start from scratch
     train_dataset_path: str = 'HuggingFaceM4/the_cauldron'
     train_dataset_name: tuple[str, ...] = ("ai2d", "aokvqa", "chart2text", "chartqa", "clevr", "cocoqa", "datikz", "diagram_image_to_text", "docvqa", "dvqa", "figureqa", "finqa", "geomverse", "hateful_memes", "hitab", "iam", "iconqa", "infographic_vqa", "intergps", "localized_narratives", "mapqa", "multihiertt", "ocrvqa", "plotqa", "raven", "rendered_text", "robut_sqa", "robut_wikisql", "robut_wtq", "scienceqa", "screen2words", "st_vqa", "tabmwp", "tallyqa", "tat_qa", "textcaps", "textvqa", "tqa", "vistext", "visual7w", "visualmrc", "vqarad", "vqav2", "vsr", "websight") # "clevr_math", "okvqa", "spot_the_diff", "nlvr2", "mimic_cgd",

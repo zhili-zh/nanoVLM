73027c8512a18ae0134f74416145f9b189c5cd1a
Kashif Rasul <kashif.rasul@gmail.com>
2025-05-23 13:18:19 +0000

    fix call to generate

--- train.py
+++ train.py
@@ -164,11 +164,9 @@ def test_mmstar(model, tokenizer, test_loader, device):
             image = batch['images'].to(device)
             input_ids = batch['input_ids'].to(device)
             labels = batch['labels'].to(device)
-            attention_mask = batch['attention_mask'].to(device)
-            
             correct_answer = tokenizer.batch_decode(labels, skip_special_tokens=True)
             
-            gen = model.generate(input_ids, image, attention_mask)
+            gen = model.generate(input_ids, image)
             model_output = tokenizer.batch_decode(gen, skip_special_tokens=True)
             
             is_correct = utils.check_multiple_choice_with_regex(model_output, correct_answer)
